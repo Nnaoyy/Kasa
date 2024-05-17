e4c5f43e7799705094259484f5627f8dc28103b7
Nnaoyy <yoann.jamain@hotmail.fr>
2024-05-17 13:34:17 +0200

    navlink active et logementcard center

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link , NavLink } from "react-router-dom";
 
 
 import './header.scss';
 
+
+function getClassName({isActive}) { 
+    if (isActive) { 
+        return "active"; // CSS class 
+    } 
+}
+
 function Header() {
+    
+    
+    
+
     return(
         <div className="header">
             <img src="/LOGODesktop.svg" alt="logo" />
        
         <nav>
-            <Link to="">Accueil</Link>
-            <Link to="about">A propos</Link>
+            <NavLink className={getClassName} to="">Accueil</NavLink>
+            <NavLink className={getClassName} to="about">A propos</NavLink>
         </nav>
        </div>
     )
